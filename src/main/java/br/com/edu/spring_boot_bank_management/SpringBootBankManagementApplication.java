170871d763f931f5a9cca220928d11e9acb7e48d
Eduardo Dragone <dragone.dev@gmail.com>
2026-06-03 15:54:58 -0300

    initial: bank-management-api setup

---
+++ src/main/java/br/com/edu/spring_boot_bank_management/SpringBootBankManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.edu.spring_boot_bank_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBankManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBankManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/edu/spring_boot_bank_management/SpringBootBankManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edu.spring_boot_bank_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBankManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
