482e5c47f9e5565abba353e1628639eee5e6efce
Eduardo Dragone <dragone.dev@gmail.com>
2026-06-04 07:58:39 -0300

    feat: initial architecture

---
+++ src/main/java/br/com/edu/spring_boot_bank_management/database/model/AccountHolderEntity.java
@@ -0,0 +1,29 @@
+package br.com.edu.spring_boot_bank_management.database.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name = "account_holder")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class AccountHolderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String CPF;
+
+
+
+}
